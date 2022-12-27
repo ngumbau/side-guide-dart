3a69b14b0ab9de800cd4ef6a2e00affff9ce920a
Kristopher Kivutha <kkibwana@gmail.com>
2022-12-27 13:17:33 +0300

    added constants

---
+++ final_const.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final int population = 1200000;
+  print(population);
+
+  const emptyList = [];
+  print(emptyList);
+
+  const String name = 'Jefferson';
+  print(name);
+}
